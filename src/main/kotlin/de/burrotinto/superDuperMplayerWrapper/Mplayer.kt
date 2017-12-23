9bd228a267c028ca4891a81144b8b8110293a038
Florian Klinger <derduke@posteo.de>
2017-12-23 08:26:29 +0100

    Getter and Setter in Kotlin Style

--- src/main/kotlin/de/burrotinto/superDuperMplayerWrapper/Mplayer.kt
+++ src/main/kotlin/de/burrotinto/superDuperMplayerWrapper/Mplayer.kt
@@ -2,8 +2,8 @@ package de.burrotinto.superDuperMplayerWrapper.wrapper
 
 import java.io.BufferedReader
 import java.io.BufferedWriter
+import java.io.File
 import java.nio.charset.Charset
-import java.util.*
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.Condition
 import java.util.concurrent.locks.ReentrantLock
@@ -11,7 +11,7 @@ import java.util.concurrent.locks.ReentrantLock
 /**
  * Created by Florian Klinger on 16.12.17, 15:51.
  */
-class Mplayer(vararg options: String) {
+open class Mplayer(vararg options: String) {
     companion object {
         val WAITING_TIME = 1000L
     }
@@ -45,8 +45,11 @@ class Mplayer(vararg options: String) {
             it.condition.signalAll()
             list.add(it)
         }
-        conditions.removeAll(list)
-
+        if (list.isNotEmpty()) {
+            conditions.removeAll(list)
+        } else {
+            notUnderstand(string)
+        }
         LOCK.unlock()
     }
 
@@ -61,7 +64,7 @@ class Mplayer(vararg options: String) {
         writer.flush()
     }
 
-    private fun executeWithReturnString(cmd: String, searchString: String): String {
+    fun executeWithReturnString(cmd: String, searchString: String): String {
         val data = Data(LOCK.newCondition(), searchString)
         LOCK.lock()
         conditions.add(data)
@@ -71,24 +74,21 @@ class Mplayer(vararg options: String) {
         return data.returnString.orEmpty()
     }
 
+    protected fun notUnderstand(string: String) {
+        println(string)
+    }
+
     fun loadfile(file: String, append: Int = 0) = execute("loadfile $file $append")
 
     fun stop() = execute("stop")
 
     fun pause() = execute("pause")
 
-    fun get_time_length(): Optional<Double> = Optional.ofNullable(executeWithReturnString("get_time_length", "ANS_LENGTH").toDoubleOrNull())
-
-    fun get_time_pos(): Optional<Double> = Optional.ofNullable(executeWithReturnString("get_time_pos", "ANS_TIME_POSITION").toDoubleOrNull())
-
-    fun loop(value: Int) = execute("loop $value", PrefixedPausedMode.PAUSING_KEEP_FORCE)
+    fun quit(value: Int = 0) = execute("quit", suffix = value.toString())
 
     fun isPaused(): Boolean = executeWithReturnString("get_property pause", "ANS_pause").trim() == "yes"
 
-    fun get_file_name(): Optional<String> = Optional.ofNullable(executeWithReturnString("get_file_name", "ANS_FILENAME"))
-
-
-    /**
+    /**y
     seek <value> [type]
 
     Seek to some place in the movie.
@@ -107,19 +107,65 @@ class Mplayer(vararg options: String) {
 
 
     //    Multiply the current speed by <value>.
-    fun speed_mult(value: Double) {
-        execute("speed_mult $value")
+    fun speed_mult(value: Double) = execute("speed_mult $value")
+
+    /**
+     * Adjust/set how many times the movie should be looped. -1 means no loop,
+    and 0 forever.
+     */
+    var loop: Int
+        get() = getProperty("loop").toInt()
+        set(value) = setProperty("loop", value.toString())
+
+    val length: Double?
+        get() = getProperty("length").toDoubleOrNull()
+
+    val bitrate: Int
+        get() = getProperty("video_bitrate").toInt()
+
+    var speed: Double?
+        get() = getProperty("speed").toDoubleOrNull()
+        set(value) = setProperty("speed", value.toString())
+
+    var fullscreen: Boolean
+        get() = getProperty("fullscreen") == "yes"
+        set(value) = setProperty("fullscreen", if (value) "1" else "0")
+
+    var timePos: Double?
+        get() = getProperty("time_pos").toDoubleOrNull()
+        set(value) = setProperty("time_pos", value.toString())
+
+    var border: Boolean
+        get() = getProperty("border") == "1"
+        set(value) = setProperty("border", if (value) "1" else "0")
+
+    var volume: Double
+        get() = getProperty("volume").toDouble()
+        set(value) = setProperty("volume", "$value")
+
+    val file: File?
+        get() {
+            return try {
+                File(getProperty("path"))
+            } catch (e: Exception) {
+                null
+            }
+        }
+
+    fun getProperty(property: String) = executeWithReturnString("get_property $property", "ANS_$property")
+
+    fun setProperty(property: String, value: String) = execute("set_property $property $value")
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("FILE: ").append(file?.toString()).append("\n")
+        sb.append("LENGTH: $length\n")
+        sb.append("POS: ").append(timePos).append("\n")
+        sb.append("SPEED: ").append(speed).append("\n")
+        sb.append("BITRATE: $bitrate\n")
+        sb.append("LOOP: $loop")
+        return sb.toString()
     }
-
-
-    //    Set the speed to <value>.
-    fun speed_set(value: Double) {
-        execute("speed_set $value")
-    }
-
-    fun isFullscreen(): Boolean = executeWithReturnString("get_vo_fullscreen", "ANS_VO_FULLSCREEN").trim() == "1"
-
-
 
     class Data(val condition: Condition, val searchString: String, var returnString: String? = null)
 }
